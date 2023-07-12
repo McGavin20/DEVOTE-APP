9c3de6098d6760f21f509f03a75c286fec126a86
Sharma <smuridzo20@gmail.com>
2023-07-12 16:08:01 +0200

    Added Utility file and Updated the Core Data Files

---
+++ DEVOTE-APP/Utility/Constant.swift
@@ -0,0 +1,22 @@
+//
+//  Constant.swift
+//  DEVOTE-APP
+//
+//  Created by Sharma on 12/07/2023.
+//
+
+import Foundation
+
+ // MARK: FORMATTER
+
+ let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK: UI
+
+
+//MARK: UX
