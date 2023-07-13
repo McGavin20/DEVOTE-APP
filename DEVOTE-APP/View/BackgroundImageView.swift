9426b97f60d491255539d86e1ced7248b62d2a11
Sharma <smuridzo20@gmail.com>
2023-07-13 16:37:42 +0200

    Background Image Update

---
+++ DEVOTE-APP/View/BackgroundImageView.swift
@@ -0,0 +1,24 @@
+//
+//  BackgroundImageView.swift
+//  DEVOTE-APP
+//
+//  Created by Sharma on 13/07/2023.
+//
+
+import SwiftUI
+
+struct BackgroundImageView: View {
+    var body: some View {
+        Image("rocket")
+            .antialiased(true)
+            .resizable()
+            .scaledToFill()
+            .ignoresSafeArea(.all)
+    }
+}
+
+struct BackgroundImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgroundImageView()
+    }
+}
--- DEVOTE-APP/View/ContentView.swift
+++ DEVOTE-APP/View/ContentView.swift
@@ -19,7 +19,7 @@ struct ContentView: View {
     
     // FETCHING DATA
     @Environment(\.managedObjectContext) private var viewContext
-
+    
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
         animation: .default)
@@ -34,7 +34,7 @@ struct ContentView: View {
             newItem.task = task
             newItem.completion = false
             newItem.id = UUID()
-
+            
             do {
                 try viewContext.save()
             } catch {
@@ -46,65 +46,74 @@ struct ContentView: View {
             hideKeyboard()
         }
     }
-
+    
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
             offsets.map { items[$0] }.forEach(viewContext.delete)
-
+            
             do {
                 try viewContext.save()
             } catch {
-               
+                
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
         }
     }
-
+    
     // MARK: BODY
     var body: some View {
         NavigationView {
-            VStack {
-                VStack(spacing: 16) {
-                    TextField("New Task", text:  $task)
+            ZStack {
+                VStack {
+                    VStack(spacing: 16) {
+                        TextField("New Task", text:  $task)
+                            .padding()
+                            .background(
+                                Color(UIColor.systemGray6)
+                            )
+                            .cornerRadius(10)
+                        
+                        Button(action: {
+                            addItem()
+                        }, label: {
+                            Spacer()
+                            Text("SAVE")
+                            Spacer()
+                        })
+                        .disabled(isButtonDisabled)
                         .padding()
+                        .font(.headline)
+                        .foregroundColor(.white)
                         .background(
-                            Color(UIColor.systemGray6)
-                        )
+                            isButtonDisabled ? Color.gray : Color.pink)
                         .cornerRadius(10)
-                    
-                    Button(action: {
-                        addItem()
-                    }, label: {
-                        Spacer()
-                        Text("SAVE")
-                        Spacer()
-                    })
-                    .disabled(isButtonDisabled)
+                    } //: VSTACK
                     .padding()
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .background(
-                        isButtonDisabled ? Color.gray : Color.pink)
-                    .cornerRadius(10)
+                    List {
+                        ForEach(items) { item in
+                            VStack(alignment: .leading) {
+                                Text(item.task ?? "")
+                                    .font(.headline)
+                                    .fontWeight(.bold)
+                                
+                                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                                    .font(.footnote)
+                                    .foregroundColor(.gray)
+                            } //: LIST ITEM
+                        }
+                        .onDelete(perform: deleteItems)
+                    } //: LIST
+                    .listStyle(InsetGroupedListStyle())
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.3), radius: 12)
+                    .padding(.vertical, 0)
+                    .frame(maxWidth: 640)
                 } //: VSTACK
-                .padding()
-                List {
-                    ForEach(items) { item in
-                        VStack(alignment: .leading) {
-                            Text(item.task ?? "")
-                                .font(.headline)
-                                .fontWeight(.bold)
-                            
-                            Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                                .font(.footnote)
-                                .foregroundColor(.gray)
-                        } //: LIST ITEM
-                    }
-                    .onDelete(perform: deleteItems)
-                } //: LIST
-            } //: VSTACK
-            .navigationBarTitle("Daily Task", displayMode: .large)
+            }//: ZSTACK
+            .onAppear() {
+                UITableView.appearance().backgroundColor = UIColor.clear
+            }
+            .navigationBarTitle("Daily Tasks", displayMode: .large)
             .toolbar {
                 #if os(iOS)
                 ToolbarItem(placement: .navigationBarTrailing) {
@@ -112,10 +121,15 @@ struct ContentView: View {
                 }
                 #endif
             }//:TOOLBAR
+            .background(
+                BackgroundImageView()
+            )
+            .background(
+                backgroundGradient.ignoresSafeArea(.all)
+            )
         } //: NAVIGATION
+        .navigationViewStyle(StackNavigationViewStyle())
     }
-
-    
 }
 
 
