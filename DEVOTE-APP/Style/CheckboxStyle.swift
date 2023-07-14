ba2cd47ec6bb10a27ea8b2026aa0a33076d6df24
Sharma <smuridzo20@gmail.com>
2023-07-14 19:07:09 +0200

    Feature: Checkbox

---
+++ DEVOTE-APP/Style/CheckboxStyle.swift
@@ -0,0 +1,32 @@
+//
+//  CheckboxStyle.swift
+//  DEVOTE-APP
+//
+//  Created by Sharma on 14/07/2023.
+//
+
+import SwiftUI
+
+struct CheckboxStyle: ToggleStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        return HStack {
+            Image(systemName: configuration.isOn ? "checkmate.circle.fill" : "circle")
+                .foregroundColor(configuration.isOn ? .pink : .primary)
+                .font(.system(size: 30, weight: .semibold, design: .rounded))
+                .onTapGesture {
+                    configuration.isOn.toggle()
+                }
+            configuration.label
+            
+        }//: HSTACK
+    }
+}
+
+struct CheckboxStyle_Previews: PreviewProvider {
+    static var previews: some View {
+        Toggle("Placeholder label", isOn: .constant(false))
+            .toggleStyle(CheckboxStyle())
+            .padding()
+            .previewLayout(.sizeThatFits)
+    }
+}
