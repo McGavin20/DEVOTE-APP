b467e36ffc27f0a105cad5b3d34e2f9384ef2845
Sharma <smuridzo20@gmail.com>
2023-07-17 15:27:45 +0200

    Devote App

--- DEVOTE-APP/View/NewTaskItemView.swift
+++ DEVOTE-APP/View/NewTaskItemView.swift
@@ -40,53 +40,52 @@ struct NewTaskItemView: View {
     }
     
     var body: some View {
-        VStack {
+      VStack {
+        Spacer()
+        
+        VStack(spacing: 16) {
+          TextField("New Task", text: $task)
+            .foregroundColor(.pink)
+            .font(.system(size: 24, weight: .bold, design: .rounded))
+            .padding()
+            .background(
+              isDarkMode ? Color(UIColor.tertiarySystemBackground) : Color(UIColor.secondarySystemBackground)
+            )
+            .cornerRadius(10)
+          
+          Button(action: {
+            addItem()
+            playSound(sound: "sound-ding", type: "mp3")
+            feedback.notificationOccurred(.success)
+          }, label: {
             Spacer()
-            
-            VStack(spacing: 16) {
-                TextField("New Task", text:  $task)
-                    .foregroundColor(.pink)
-                    .font(.system(size: 24, weight: .bold, design: .rounded))
-                    .padding()
-                    .background(
-                        isDarkMode ? Color(.tertiarySystemBackground) : Color(UIColor.secondarySystemGroupedBackground)
-                    )
-                    .cornerRadius(10)
-                
-                Button(action: {
-                    addItem()
-                    playSound(sound: "sound-ding", type: "mp3")
-                    feedback.notificationOccurred(.success)
-                }, label: {
-                    Spacer()
-                    Text("SAVE")
-                        .font(.system(size: 24, weight: .bold, design: .rounded))
-                    Spacer()
-                })
-                .disabled(isButtonDisabled)
-                .onTapGesture {
-                    if isButtonDisabled {
-                        playSound(sound: "sound-tap", type: "mp3")
-                    }
-                }
-                .padding()
-                .font(.headline)
-                .foregroundColor(.white)
-                .background(
-                    isButtonDisabled ? Color.blue : Color.pink)
-                .cornerRadius(10)
-            } //: VSTACK
-            .padding(.horizontal)
-            padding(.vertical, 20)
-                .background(
-                    isDarkMode ? Color(.secondarySystemGroupedBackground) : Color.white
-                )
-                .cornerRadius(16)
-                .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.65), radius: 24)
-                .padding()
-        }
+            Text("SAVE")
+              .font(.system(size: 24, weight: .bold, design: .rounded))
+            Spacer()
+          })
+          .disabled(isButtonDisabled)
+          .onTapGesture {
+            if isButtonDisabled {
+              playSound(sound: "sound-tap", type: "mp3")
+            }
+          }
+          .padding()
+          .foregroundColor(.white)
+          .background(isButtonDisabled ? Color.blue : Color.pink)
+          .cornerRadius(10)
+        } //: VSTACK
+        .padding(.horizontal)
+        .padding(.vertical, 20)
+        .background(
+          isDarkMode ? Color(UIColor.secondarySystemBackground) : Color.white
+        )
+        .cornerRadius(16)
+        .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.65), radius: 24)
+        .frame(maxWidth: 640)
+      } //: VSTACK
+      .padding()
     }
-}
+  }
 
 struct NewTaskItemView_Previews: PreviewProvider {
     static var previews: some View {
