02b0d17820357b4fbc671f79c6166eae32fa5f8f
Sharma <smuridzo20@gmail.com>
2023-07-17 14:37:09 +0200

    Feature: Custom Visual Effect

--- DEVOTE-APP/View/ContentView.swift
+++ DEVOTE-APP/View/ContentView.swift
@@ -102,9 +102,15 @@ struct ContentView: View {
                     .padding(.vertical, 0)
                     .frame(maxWidth: 640)
                 } //: VSTACK
+                .blur(radius: showNewTaskItem ? 8.0 : 0, opaque: false)
+                .transition(.move(edge: .bottom))
+                .animation(.easeOut(duration: 0.5))
                 
                 if showNewTaskItem {
-                    BlankView()
+                    BlankView(
+                        backgroundColor: isDarkMode ? Color.black : Color.gray,
+                        backgroundOpacity: isDarkMode ? 0.3 : 0.5
+                    )
                         .onTapGesture {
                             withAnimation() {
                                 showNewTaskItem = false
@@ -120,6 +126,7 @@ struct ContentView: View {
             .navigationBarHidden(true)
             .background(
                 BackgroundImageView()
+                    .blur(radius: showNewTaskItem ? 8.0 : 0, opaque: false)
             )
             .background(
                 backgroundGradient.ignoresSafeArea(.all)
